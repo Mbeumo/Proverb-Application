f8c44231357a5d80b253a5d14bfc512a7854a7f4
Mbeumo <etiennedonfang2@gmail.com>
2025-02-26 09:51:29 +0100

    added the notefile view but incomplete

---
+++ lib/notefile.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:proverbapp/services/authservice.dart';
+import 'package:proverbapp/services/translation.dart';
+class NotefilePage extends StatefulWidget {
+  const NotefilePage({super.key});
+
+  @override
+  NotefilePage createState() => NotefilePage();
+}
+
+class _SignUpPageState extends State<NotefilePage> {
+  final AuthService objauthservice = AuthService();
+  final TextEditingController _noteController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(AppLocalizations.of(context)!.translate('notes')!,)),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            TextField(
+              controller: _noteController,//chnge email to the correspknding json key
+              decoration: InputDecoration(labelText: AppLocalizations.of(context)!.translate('email')!,),
+            ),
+
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () async{
+                //await objauthservice.signUp(
+                   // context:context, email:_emailController.text, password:_passwordController.text);
+               // );
+                //await objauthservice.signIn(
+                  //  context: context, email:_emailController.text, password:_passwordController.text
+               // );
+
+              },
+              //this is to validate the note
+              child: Text( AppLocalizations.of(context)!.translate('sign_up')!,),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
